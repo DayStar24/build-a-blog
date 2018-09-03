5ec784d90117b6f829dfe1fef0cb1ddb55c16570
zedboson <walters.ralph@outlook.com>
2018-09-03 14:21:04 -0500

    Working Blogger Web App

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 
 app = Flask(__name__)
@@ -15,27 +16,43 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(600))
+    pub_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, pub_date):
 
         self.title = title
         self.body = body
+        self.pub_date = pub_date
 
 
 @app.route('/blog', methods=['GET'])
 def blogs():
 
-    blogs = Blog.query.all()
-
     template = ''
+    
+    #blogs = db.session.query(Blog).order_by(Blog.pub_date.desc())
+    #blog = request.args.get('id')
 
-    template = render_template('blogs.html', h1_text='Build a Blog', blogs=blogs)
+    if request.args.get('id'):
+        template = render_template (
+            'blog.html', 
+            h1_text = Blog.query.get(request.args.get('id')).title, 
+            p_text = Blog.query.get(request.args.get('id')).body
+        )
+    else:
+        template = render_template(
+            'blogs.html', 
+            h1_text = 'Build a Blog', 
+            blogs = db.session.query(Blog).order_by(Blog.pub_date.desc())
+        )
 
     return template
 
 
 @app.route('/newpost', methods=['POST','GET'])
 def new_post():
+
+    pub_date = datetime.datetime.now()
 
     title = ''
     body = ''
@@ -54,12 +71,18 @@ def new_post():
             error_body = 'Write a blog...'
 
         if error_title or error_body:
-            template = render_template('new_post.html', h1_text='Add a Blog Entry', title=title, body=body, error_title=error_title, error_body=error_body)
+            template = render_template (
+                'new_post.html', 
+                title = title, 
+                body = body, 
+                error_title = error_title, 
+                error_body = error_body
+            )
         else:
-            blog = Blog(title, body)
+            blog = Blog(title, body, pub_date)
             db.session.add(blog)
             db.session.commit()
-            template = render_template('post.html', h1_text=blog.title, p_text=blog.body)
+            template = redirect('/blog?id=' + str(blog.id))
     else:
         template = render_template('new_post.html')
 
